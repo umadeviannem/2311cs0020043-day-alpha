4813167f3d9ff9d27104560f138f7a3f6cc8fd35
umadeviannem <umadeviannem38@gmail.com>
2025-01-08 22:06:38 +0530

    Add files via upload

---
+++ 2311cs020043-day2.py
@@ -0,0 +1,39 @@
+# day2 
+ # List Example
+
+colors = ["red", "blue", "green", "yellow", "purple"]
+
+print(colors[1])  
+print(colors[3])  
+
+
+colors[2] = "cyan" 
+print(colors)  
+
+print(colors[2:])
+
+#Dictionary Example
+
+# Creating a dictionary
+
+person = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York",
+    "profession": "Engineer",
+    "hobby": "Photography"
+}
+
+print(person["name"])  
+print(person["hobby"])  
+print(person.get("age"))  
+for key, value in person.items():
+    print(f"{key}: {value}")
+
+# Tuple Example
+animals = ("lion", "tiger", "elephant", "giraffe", "zebra")
+
+
+print(animals[0])  
+print(animals[-1])  
+print(animals[1:4])
