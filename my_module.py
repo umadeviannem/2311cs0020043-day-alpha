cb06f7dff69be65179d96c4c1a814191417a60c2
umadeviannem <umadeviannem38@gmail.com>
2025-01-09 13:19:29 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+greet = lambda name : print(f"Good morning {name}!")
+
+
+# In[3]:
+
+
+greet("Sowmya")
+
+
+# In[4]:
+
+
+#product of three numbers
+product = lambda a,b,c : a*b*c
+
+
+# In[5]:
+
+
+product(35,43,53)
+
+
+# In[7]:
+
+
+even = lambda L : [x for x in L if x%2 == 0]
+
+
+# In[8]:
+
+
+my_list = [100, 3, 9, 38, 43, 56, 20]
+even(my_list)
+
+
+# In[ ]:
+
+
+
+
